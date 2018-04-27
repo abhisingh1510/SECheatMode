cd93162615c40cf336256697fe15d3a20ae0a346
abhisingh1510 <abhisona2u@gmail.com>
2018-04-27 13:30:29 +0530

    Minor changes

--- app/src/main/java/com/abhisheksingh/ifbservices/EmployeePage.java
+++ app/src/main/java/com/abhisheksingh/ifbservices/EmployeePage.java
@@ -5,19 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 
 public class EmployeePage extends AppCompatActivity {
 
     Button logout;
     RatingBar rb;
+    EditText name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_page);
         logout=(Button)findViewById(R.id.logout_employee_btn);
         rb=(RatingBar)findViewById(R.id.ratingBar_employee);
-        rb.setVisibility(View.INVISIBLE);
+        name=(EditText)findViewById(R.id.employeeName_text);
+        //rb.setVisibility(View.INVISIBLE);
+        rb.setRating(3.5f);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
